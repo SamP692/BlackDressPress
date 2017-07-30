ab95c8fd64aa1eda672ed069bcd889c3e5c6f35a
Sam Peterson <samuel.e.peterson@gmail.com>
2017-07-30 16:14:29 -0400

    scaffold base organization and add link config

---
+++ src/ui/Nav/linkConfig.js
@@ -0,0 +1,26 @@
+export default {
+  home: {
+    display: 'home',
+    location: '/',
+  },
+  news: {
+    display: 'news',
+    location: '/news',
+  },
+  comingSoon: {
+    display: 'coming soon',
+    location: '/soon',
+  },
+  about: {
+    display: 'about',
+    location: '/about',
+  },
+  store: {
+    display: 'store',
+    location: '/store',
+  },
+  externalLinks: {
+    display: 'friends',
+    location: 'friends',
+  },
+};
